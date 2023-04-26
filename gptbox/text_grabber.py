6ab2d80f835e352fbd5451c2d6c39c88da187773
zhuangjun1981 <wood_stocker@hotmail.com>
2023-04-26 02:55:10 -0700

    more on text_grabber

--- gptbox/text_grabber.py
+++ gptbox/text_grabber.py
@@ -1,22 +1,50 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = "https://www.space.com/spacex-starship-launch-debris-terrifying"
-html = urlopen(url).read()
-soup = BeautifulSoup(html, features="html.parser")
+def grab_text_from_url(url):
 
-# kill all script and style elements
-for script in soup(["script", "style"]):
-    script.extract()    # rip it out
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, features="html.parser")
 
-# get text
-text = soup.get_text()
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
 
-# break into lines and remove leading and trailing space on each
-lines = (line.strip() for line in text.splitlines())
-# break multi-headlines into a line each
-chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-# drop blank lines
-text = '\n'.join(chunk for chunk in chunks if chunk)
+    # get text
+    text = soup.get_text()
 
-print(text)
+    # # break into lines and remove leading and trailing space on each
+    # lines = (line.strip() for line in text.splitlines())
+    # # break multi-headlines into a line each
+    # chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # # drop blank lines
+    # text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    return text
+
+
+class TextCleaner(object):
+
+    def __init__(self):
+        return
+    
+    @staticmethod
+    def basic_cleaning(txt):
+
+        # break into lines and remove leading and trailing space on each
+        lines = (line.strip() for line in txt.splitlines())
+        # break multi-headlines into a line each
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        # drop blank lines
+        txt = '\n'.join(chunk for chunk in chunks if chunk)
+
+        return txt
+
+
+if __name__ == "__main__":
+    url = "https://www.space.com/spacex-starship-launch-debris-terrifying"
+    
+    txt = grab_text_from_url(url=url)
+    txt_cleaner = TextCleaner()
+    txt = TextCleaner.basic_cleaning(txt=txt)
+    print(txt)
