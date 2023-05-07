fbb3b49d37055cd8ed03eb21487683f5514d8d5a
zhuangjun1981 <wood_stocker@hotmail.com>
2023-05-06 18:43:59 -0700

    # modified:   gptbox/scripts/translate_spacedotcom.py
    structural change done.

--- gptbox/scripts/translate_spacedotcom.py
+++ gptbox/scripts/translate_spacedotcom.py
@@ -13,11 +13,11 @@ save_folder = r"G:\temp"
 # url = "https://spacenews.com/esa-troubleshooting-juice-radar-antenna/"
 url = "https://www.space.com/voyager-2"
 
-text_dict = cg.get_text_from_html(url=url)
-h5_path = dt.save_html_content(text_dict=text_dict, folder=save_folder)
-dt.translate_h5_file(h5_path=h5_path, 
-                     model="gpt-3.5-turbo",
-                     temperature=0)
+# text_dict = cg.get_text_from_html(url=url)
+# h5_path = dt.save_html_content(text_dict=text_dict, folder=save_folder)
+# dt.translate_h5_file(h5_path=h5_path, 
+#                      model="gpt-3.5-turbo",
+#                      temperature=0)
 
-h5_path = r"G:\temp\2023-04-21_space.com.h5"
+h5_path = r"G:\temp\2022-08-17_space.com.h5"
 dt.save_text_files(h5_path)
