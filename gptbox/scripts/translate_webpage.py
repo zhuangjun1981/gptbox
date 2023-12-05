2210b5d1d996d27d1b42d5fcc3e26214ee87879f
zhuangjun1981 <wood_stocker@hotmail.com>
2023-12-05 08:52:32 -0800

    One more link

--- gptbox/scripts/translate_webpage.py
+++ gptbox/scripts/translate_webpage.py
@@ -34,7 +34,9 @@ save_folder = r"G:\temp"
 # url = "https://www.space.com/expert-voice-what-is-an-attosecond"
 # url = "https://www.space.com/spacex-starship-second-test-flight-launch-explodes"
 # url = "https://www.space.com/space-force-x-37b-spacex-falcon-heavy-1st-launch-dec-2023"
-url = "https://spacenews.com/satellite-imagery-reveals-explosion-at-chinas-jiuquan-spaceport/"
+# url = "https://spacenews.com/satellite-imagery-reveals-explosion-at-chinas-jiuquan-spaceport/"
+# url = "https://spacenews.com/china-makes-progress-on-raptor-like-engines-for-super-heavy-rocket/"
+url = "https://www.space.com/what-are-radio-galaxies"
 
 
 text_dict = cg.get_text_from_html(url=url)
