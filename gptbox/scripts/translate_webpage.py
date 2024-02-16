763dbc63abebcc3b01e214289f56bd218ae8a833
zhuangjun1981 <wood_stocker@hotmail.com>
2024-02-15 19:35:29 -0800

    Update scripts

--- gptbox/scripts/translate_webpage.py
+++ gptbox/scripts/translate_webpage.py
@@ -48,7 +48,8 @@ save_folder = r"G:\temp"
 # url = "https://spacenews.com/moores-law-space/"
 # url = "https://spacenews.com/chinas-change-6-probe-arrives-at-spaceport-for-first-ever-lunar-far-side-sample-mission/"
 # url = "https://www.space.com/solar-maximum-in-sight-but-scientists-will-have-to-wait-seven-months-after-it-occurs-to-officially-declare-it"
-url = "https://www.space.com/nasa-jpl-layoffs-february-2024-mars-sample-return"
+# url = "https://www.space.com/nasa-jpl-layoffs-february-2024-mars-sample-return"
+url = "https://www.space.com/nasa-voyager-spacecraft-mission-deep-space-update"
 
 
 text_dict = cg.get_text_from_html(url=url)
