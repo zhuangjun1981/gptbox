3960508da4ed7329680e6a2ae1b26516715b1ef6
zhuangjun1981 <wood_stocker@hotmail.com>
2024-03-14 13:41:49 -0700

    Add four more links

--- gptbox/scripts/translate_webpage.py
+++ gptbox/scripts/translate_webpage.py
@@ -51,7 +51,8 @@ save_folder = r"G:\temp"
 # url = "https://www.space.com/nasa-jpl-layoffs-february-2024-mars-sample-return"
 # url = "https://www.space.com/nasa-voyager-spacecraft-mission-deep-space-update"
 # url = "https://www.space.com/mars-ingenuity-helicopter-blade-mars-perseverance-photo"
-url = "https://www.space.com/spacex-starship-flight-3-launch-what-time"
+# url = "https://www.space.com/spacex-starship-flight-3-launch-what-time"
+url = "https://www.space.com/spacex-starship-third-test-flight-launch"
 
 
 text_dict = cg.get_text_from_html(url=url)
