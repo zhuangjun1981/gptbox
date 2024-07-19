fb1102cac5c0980c1ebada5b54bd6872b27737f4
jun-wispr <jun@wispr.ai>
2024-07-19 08:43:46 -0700

    Add one new url

--- gptbox/scripts/translate_webpage.py
+++ gptbox/scripts/translate_webpage.py
@@ -83,7 +83,8 @@ save_folder = r"F:\webpage_translation"
 # url = "https://spacenews.com/china-reschedules-planetary-defense-mission-for-2027-launch/"
 # url = "https://www.space.com/spacex-rocket-failure-nasa-astronaut-launch-schedule-iss"
 # url = "https://www.space.com/iss-deorbit-destroy-spacex-vehicle-18-months"
-url = "https://spacenews.com/china-prepares-to-launch-new-long-march-12-rocket/"
+# url = "https://spacenews.com/china-prepares-to-launch-new-long-march-12-rocket/"
+url = "https://spacenews.com/enhanced-dragon-spacecraft-to-deorbit-the-iss-at-the-end-of-its-life/"
 
 text_dict = cg.get_text_from_html(url=url)
 h5_path = dt.save_html_content(text_dict=text_dict, folder=save_folder)
