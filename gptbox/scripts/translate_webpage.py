a441bee0393b91c650119313183c680ca87a1ec0
jun-wispr <jun@wispr.ai>
2024-08-06 15:49:21 -0700

    Add two urls

--- gptbox/scripts/translate_webpage.py
+++ gptbox/scripts/translate_webpage.py
@@ -95,7 +95,8 @@ save_folder = r"F:\webpage_translation"
 # url = "https://spacenews.com/chinese-scientists-outline-major-cislunar-space-infrastructure-project/"
 # url = "https://www.space.com/boeing-starliner-nasa-astronauts-not-stranded-space"
 # url = "https://spacenews.com/boeing-takes-additional-125-million-loss-on-starliner/"
-url = "https://www.space.com/settling-other-planets-will-mean-human-genome-editing"
+# url = "https://www.space.com/settling-other-planets-will-mean-human-genome-editing"
+url = "https://spacenews.com/china-launches-first-satellites-for-thousand-sails-megaconstellation/"
 
 text_dict = cg.get_text_from_html(url=url)
 h5_path = dt.save_html_content(text_dict=text_dict, folder=save_folder)
