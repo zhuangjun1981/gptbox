c951e3136771686114f8f9e40190c9cb699fffda
Jun Zhuang <wood_stocker@hotmail.com>
2024-11-18 18:48:46 -0800

    Reorganize .env keys

--- gptbox/translate.py
+++ gptbox/translate.py
@@ -1,9 +1,9 @@
 import os, openai
+from dotenv import load_dotenv
 
-curr_folder = os.path.dirname(os.path.realpath(__file__))
 
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key_path = os.path.join(curr_folder, "meta", "api_key.txt")
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def run_gpt(prompt, model="gpt-3.5-turbo", **kwargs):
@@ -64,19 +64,4 @@ def translate_long_text(text, max_len=1500, **kwargs):
 
 
 if __name__ == "__main__":
-    # import h5py
-    # file_path = r"G:\temp\2023-04-21_space.com.h5"
-    # ff = h5py.File(file_path, 'r')
-    # body = ff['body'][()].decode()
-    # title = ff['title'][()].decode()
-
-    # print(title)
-    # prompt_t = get_simple_translate_prompt(txt=title)
-    # print(run_gpt(prompt=prompt_t))
-
-    fp = r"G:\temp\2022-08-17_space.com_eng.txt"
-    with open(fp, "r", encoding="UTF-8") as f:
-        txt = f.readlines()
-    txt = "\n".join(txt)
-    blocks = break_text_into_blocks(txt)
-    [print(f"```{b}```") for b in blocks]
+    print(os.getenv("OPENAI_API_KEY"))
